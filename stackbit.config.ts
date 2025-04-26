c462699e9f61edcf77c4ca349412b42b46d9b96c
Proyecto Flower <contacto@proyectoflower.com>
2025-04-26 16:26:49 -0400

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,21 +1,27 @@
-import { SanityContentSource } from '@stackbit/cms-sanity';
-import path from 'path';
-
-import { defineStackbitConfig } from '@stackbit/types';
+// stackbit.config.ts
+import { defineStackbitConfig } from "@stackbit/types";
+import { SanityContentSource } from "@stackbit/cms-sanity";
 
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "custom",
-    "contentSources": [
-        new SanityContentSource({
-            projectId: process.env.SANITY_PROJECT_ID!,
-            token: process.env.SANITY_ACCESS_TOKEN!,
-            dataset: process.env.SANITY_DATASET || 'production',
-            rootPath: __dirname,
-            studioPath: path.resolve(__dirname, 'studio'),
-            studioUrl: process.env.SANITY_STUDIO_URL || ''
-        }),
-    ],
-    "postInstallCommand": "npm i --no-save @stackbit/types @stackbit/cms-sanity"
-})
+  stackbitVersion: "~0.6.0",
+  ssgName: "nextjs",
+  nodeVersion: "16",
+  contentSources: [
+    new SanityContentSource({
+      rootPath: __dirname,
+      studioPath: path.join(__dirname, "studio"),
+      studioUrl: "https://project.sanity.studio",
+      projectId: process.env.SANITY_PROJECT_ID!,
+      token: process.env.SANITY_ACCESS_TOKEN!,
+      dataset: process.env.SANITY_DATASET || "production"
+    })
+  ],
+  mapModels: ({ models }) => {
+    return models.map(model => {
+      if (model.name === "page") {
+        return { ...model, type: "page", urlPath: "/{slug}" };
+      }
+      return model;
+    });
+  }
+});
